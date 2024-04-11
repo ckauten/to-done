7a9eb2f4cf2de4ff411118ef4639d4ba4d09f964
Cameron Kauten <cameron.kauten@gmail.com>
2024-04-11 13:43:53 -0700

    updated dependencies and commented wrote comments

--- controllers/todos.js
+++ controllers/todos.js
@@ -5,31 +5,44 @@ module.exports = {
     console.log(req.user);
     try {
       const todoItems = await Todo.find({ userId: req.user.id });
+      //creates variable for the items in the todo matching the userId
       const itemsLeft = await Todo.countDocuments({ userId: req.user.id, completed: false });
+      //counts the items in the db and assigns the number to a variable
       res.render('todos.ejs', { todos: todoItems, left: itemsLeft, user: req.user });
+      //renders out the todoItems variable (containing the items) as well as the amount of items left to do (contained in the itemsLeft variable)
     } catch (err) {
       console.log(err);
+      //catches any errors
     }
   },
   createTodo: async (req, res) => {
     try {
       await Todo.create({ todo: req.body.todoItem, completed: false, userId: req.user.id });
+      //creates an item in the db with containing the item (pulled from the body), the completed property (boolean) and the userId attached
       console.log('Todo has been added!');
+      //console logs it
       res.redirect('/todos');
+      //redirects to /todos, which makes a get request to pull the new todo, and reloads
     } catch (err) {
       console.log(err);
+      //console logs any errors
     }
   },
   markComplete: async (req, res) => {
     try {
       await Todo.findOneAndUpdate(
         { _id: req.body.todoIdFromJSFile },
+        //todoIdFromJSFile comes from the client side js file, which defines it using parentNode.dataset.id
         {
           completed: true,
+          //changes completed property to true
         }
       );
+      //async function to find the item in the db with the matching id and change the completed property to true
       console.log('Marked Complete');
+      //console logs completion
       res.json('Marked Complete');
+      //responds with 'Marked Complete'
     } catch (err) {
       console.log(err);
     }
@@ -42,27 +55,27 @@ module.exports = {
           completed: false,
         }
       );
+      //finds document with matching id and changes completed property to false
       console.log('Marked Incomplete');
+      //console logs it
       res.json('Marked Incomplete');
+      //responds to the fetch request
     } catch (err) {
       console.log(err);
     }
   },
   deleteTodo: async (req, res) => {
     console.log(req.body.todoIdFromJSFile);
+    //immediatley console logs the todoId
     try {
       await Todo.findOneAndDelete({ _id: req.body.todoIdFromJSFile });
+      //tries to find the document with the matching id, provided by the req body
       console.log('Deleted Todo');
+      //console logs it
       res.json('Deleted It');
+      //responds to client side js
     } catch (err) {
       console.log(err);
     }
   },
-
-  //
-  //
-  //HANDLES AI REQ
-  //
-  //
-  //
 };
--- public/js/main.js
+++ public/js/main.js
@@ -3,21 +3,30 @@ const todoItem = document.querySelectorAll('span.not');
 const todoComplete = document.querySelectorAll('span.completed');
 
 Array.from(deleteBtn).forEach((el) => {
+  //creates an array from all the elements with the class of del
   el.addEventListener('click', deleteTodo);
+  //if clicked, it will run the client side async function 'deleteTodo'
 });
 
 Array.from(todoItem).forEach((el) => {
+  //creates an array containing each todoItem
   el.addEventListener('click', markComplete);
+  //forEach puts an event listener on them, listening for a click and firing the markComplete function below
 });
 
 Array.from(todoComplete).forEach((el) => {
+  //creates an array from the new todoComplete variable created from all the elements with the newly assigned property of 'completed'
   el.addEventListener('click', markIncomplete);
+  //adds event listener for a click and runs the markIncomplete client side async function
 });
 
 //AI FETCH FUNCTION
 document.querySelector('#submit').addEventListener('click', async function sendAiReq() {
+  //event listener and function combined into one line
   const prompt = document.querySelector('#textarea').value;
+  //prompt variable created using what was inside the text area
   const response = await fetch('todos/generateText', {
+    //makes a fetch request to the todos controller which fires the generateText function
     method: 'post',
     headers: { 'Content-type': 'application/json' },
     body: JSON.stringify({ prompt: prompt }),
@@ -25,8 +34,12 @@ document.querySelector('#submit').addEventListener('click', async function sendA
 
   if (response.ok) {
     const data = await response.json();
-    document.getElementById('output').textContent = data.text;
-    console.log(data.text);
+    const genRes = data.text;
+    //variable created from response (genRes = Generated response)
+    document.getElementById('output').textContent = genRes;
+    //renders what was in the response into the output field
+    console.log(genRes);
+    //console logs the data
   } else {
     console.error(`"Error from server"`);
   }
@@ -34,8 +47,10 @@ document.querySelector('#submit').addEventListener('click', async function sendA
 
 async function deleteTodo() {
   const todoId = this.parentNode.dataset.id;
+  //creates variable containing the id from the parentNode
   try {
     const response = await fetch('todos/deleteTodo', {
+      //tries to make a fetch request to the delete router with the method of 'deleteTodo'
       method: 'delete',
       headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({
@@ -43,8 +58,11 @@ async function deleteTodo() {
       }),
     });
     const data = await response.json();
+    //creates variable for response
     console.log(data);
+    //console logs the new variable
     location.reload();
+    //reloads the page, making a get request and displaying new items
   } catch (err) {
     console.log(err);
   }
@@ -52,8 +70,10 @@ async function deleteTodo() {
 
 async function markComplete() {
   const todoId = this.parentNode.dataset.id;
+  //variable for todoId, pulling it from the parent node which contains its ID
   try {
     const response = await fetch('todos/markComplete', {
+      //makes a fetch request to the todos router which runs the markComplete method
       method: 'put',
       headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({
@@ -61,8 +81,11 @@ async function markComplete() {
       }),
     });
     const data = await response.json();
+    //awaits the data back from the fetch request
     console.log(data);
+    //consoles logs the data
     location.reload();
+    //reloads the page
   } catch (err) {
     console.log(err);
   }
@@ -70,8 +93,11 @@ async function markComplete() {
 
 async function markIncomplete() {
   const todoId = this.parentNode.dataset.id;
+  //the function creates a variable containing the id from the parentNode dataId
+  //the dataId value is from the database and assigned in using the template literal
   try {
     const response = await fetch('todos/markIncomplete', {
+      //fetch request to the todos router firing the markIncomplete method
       method: 'put',
       headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({
@@ -79,8 +105,11 @@ async function markIncomplete() {
       }),
     });
     const data = await response.json();
+    // variable is created from response
     console.log(data);
+    //variable is console logged
     location.reload();
+    //reload
   } catch (err) {
     console.log(err);
   }
